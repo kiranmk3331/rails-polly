843dee8f30e01851b94d04ea831b45c1af7d4759
kiranmk3331 <kiranmk3331@gmail.com>
2021-04-08 15:52:56 +0530

    Added CRUD operation

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,40 @@
+class PollsController < ApplicationController
+  def index
+    polls = Poll.all
+    render status: :ok, json: { polls: polls }
+  end
+
+  def show
+    poll = Poll.find(params[:id])
+    options = poll.options
+    render status: :ok, json: { poll: poll, options: options }
+  end
+
+  def create
+    @poll = Poll.create(poll_params)
+    @poll.options.create(poll_options[:options])
+    render status: :ok, json: { poll: @poll, options: @poll.options }
+  end
+
+  def destroy
+    poll = Poll.find(params[:id])
+    poll.destroy
+  end
+
+  def update
+    poll = Poll.find(params[:id])
+    poll.update(poll_params)
+    poll.options.upsert_all(poll_options[:options])
+    render status: :ok, json: { poll: poll, options: poll.options }
+  end
+
+  private
+
+  def poll_params
+    params.require(:poll).permit(:title)
+  end
+
+  def poll_options
+    params.require(:poll).permit(options: [:id, :title, :created_at, :updated_at])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/polls", to: "polls#index"
+  # get "/polls/:id", to: "polls#show"
+  # post "/polls", to: "polls#create"
+  # delete "/polls/:id", to: "polls#destroy"
+
+  resources :polls
 end
