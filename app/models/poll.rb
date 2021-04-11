4a95cff25961f4204d482d052ff29906e2a6860a
kiranmk3331 <kiranmk3331@gmail.com>
2021-04-11 11:58:15 +0530

    Added user model and migrations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,5 @@
 class Poll < ApplicationRecord
+  belongs_to :user
   has_many :options, dependent: :delete_all
+  validates :title, presence: true, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_103350) do
+ActiveRecord::Schema.define(version: 2021_04_11_062030) do
 
   create_table "options", force: :cascade do |t|
     t.text "title"
@@ -25,7 +25,19 @@ ActiveRecord::Schema.define(version: 2021_04_08_103350) do
     t.text "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "options", "polls"
+  add_foreign_key "polls", "users", on_delete: :cascade
 end
